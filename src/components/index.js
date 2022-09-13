744ceeed935113c6a05436db5b5d49f96e8ac6f8
Uttam <uttamakwana4503@gmail.com>
2022-09-13 08:03:06 +0530

    Added file structure and assets folder

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Navbar} from './Navbar/Navbar';
+export {default as Scheme} from './Scheme/Scheme';
+export {default as About} from './About/About';
+export {default as Footer} from './Footer/Footer';
